76b8fb773924f61925b3acc3be67275c955fc074
Alhussiny <eng.elhoseny.osama@gmail.com>
2023-02-23 18:56:40 +0200

    annnotate employee model class

--- Shared/Employee.cs
+++ Shared/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,12 @@ namespace EmployeeManagementBlazor.Shared
     public class Employee
     {
         public int EmployeeId { get; set; }
+        [Required]
+        [MinLength(2)]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
         public DateTime DateOfBrith { get; set; }
         public Gender Gender { get; set; }
