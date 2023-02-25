a399b90e2c0851171ba1b912f3670cbf6c8c9655
Alhussiny <eng.elhoseny.osama@gmail.com>
2023-02-25 19:11:05 +0200

    departmentsController

---
+++ Server/Controllers/DepartmentsController.cs
@@ -0,0 +1,54 @@
+﻿using EmployeeManagementBlazor.Server.Model;
+using EmployeeManagementBlazor.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementBlazor.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DepartmentsController : Controller
+    {
+        private readonly IDepartmentRepository departmentRepository;
+
+        public DepartmentsController(IDepartmentRepository departmentRepository)
+        {
+            this.departmentRepository = departmentRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetDepartments()
+        {
+            try
+            {
+                return Ok(await departmentRepository.GetDepartments());
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal DB Error");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Department>> GetDepartment(int id)
+        {
+            try
+            {
+                var department = await departmentRepository.GetDepartment(id);
+                if (department == null)
+                {
+                    return NotFound();
+                }
+                return department;
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal DB Error");
+            }
+        }
+    }
+}
