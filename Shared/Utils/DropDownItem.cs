34be5ff46f0a6b26a82b82db37552a0a43d3f0e8
Alhussiny <eng.elhoseny.osama@gmail.com>
2023-02-27 20:42:08 +0200

    drop down list in filter for grid

---
+++ Shared/Utils/DropDownItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementBlazor.Shared.Utils
+{
+    public class DropDownItem
+    {
+        public string Value { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ Shared/Utils/EnumHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementBlazor.Shared.Utils
+{
+    public class EnumHelper
+    {
+        public static List<DropDownItem> EnumToList<T>(string intialValue, string intialText)
+        {
+            var list = new List<DropDownItem>();
+            if (!string.IsNullOrEmpty(intialText) && !string.IsNullOrEmpty(intialValue))
+            {
+                DropDownItem ddi = new DropDownItem { 
+                Text = intialText,
+                Value = intialValue
+                };
+                list.Add(ddi);
+            }
+
+            var values = Enum.GetValues(typeof(T)).Cast<T>().ToList();
+            for (int i = 0; i < Enum.GetNames(typeof(T)).Length; i++)
+            {
+                DropDownItem ddi = new DropDownItem
+                {
+                    Text = Enum.GetNames(typeof(T))[i],
+                    Value = values[i].ToString()
+                };
+                list.Add(ddi);
+            }
+            return list;
+        }
+    }
+}
